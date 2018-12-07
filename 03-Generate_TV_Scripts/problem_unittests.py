b5e767bd83f638bdf82b4c319f9d438fdff53e5b
JTB <j.t@free.fr>
2018-12-07 11:36:09 +0100

    file format corrected

--- 03-Generate_TV_Scripts/problem_unittests.py
+++ 03-Generate_TV_Scripts/problem_unittests.py
@@ -1,261 +1,229 @@
-<!DOCTYPE HTML>
-<html>
+from unittest.mock import MagicMock, patch
+import numpy as np
+import torch
 
-<head>
-    <meta charset="utf-8">
 
-    <title>problem_unittests.py (editing)</title>
-    <link rel="shortcut icon" type="image/x-icon" href="/jupyter/static/base/images/favicon.ico?v=97c6417ed01bdc0ae3ef32ae4894fd03">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="/jupyter/static/components/jquery-ui/themes/smoothness/jquery-ui.min.css?v=9b2c8d3489227115310662a343fce11c" type="text/css" />
-    <link rel="stylesheet" href="/jupyter/static/components/jquery-typeahead/dist/jquery.typeahead.min.css?v=7afb461de36accb1aa133a1710f5bc56" type="text/css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+class _TestNN(torch.nn.Module):
+    def __init__(self, input_size, output_size):
+        super(_TestNN, self).__init__()
+        self.decoder = torch.nn.Linear(input_size, output_size)
+        self.forward_called = False
+    
+    def forward(self, nn_input, hidden):
+        self.forward_called = True
+        output = self.decoder(nn_input)
+        
+        return output, hidden
+
+
+def _print_success_message():
+    print('Tests Passed')
+
+
+class AssertTest(object):
+    def __init__(self, params):
+        self.assert_param_message = '\n'.join([str(k) + ': ' + str(v) + '' for k, v in params.items()])
+    
+    def test(self, assert_condition, assert_message):
+        assert assert_condition, assert_message + '\n\nUnit Test Function Parameters\n' + self.assert_param_message
+
+
+def test_create_lookup_tables(create_lookup_tables):
+    test_text = '''
+        Moe_Szyslak Moe's Tavern Where the elite meet to drink
+        Bart_Simpson Eh yeah hello is Mike there Last name Rotch
+        Moe_Szyslak Hold on I'll check Mike Rotch Mike Rotch Hey has anybody seen Mike Rotch lately
+        Moe_Szyslak Listen you little puke One of these days I'm gonna catch you and I'm gonna carve my name on your back with an ice pick
+        Moe_Szyslak Whats the matter Homer You're not your normal effervescent self
+        Homer_Simpson I got my problems Moe Give me another one
+        Moe_Szyslak Homer hey you should not drink to forget your problems
+        Barney_Gumble Yeah you should only drink to enhance your social skills'''
+    
+    test_text = test_text.lower()
+    test_text = test_text.split()
+    
+    vocab_to_int, int_to_vocab = create_lookup_tables(test_text)
+    
+    # Check types
+    assert isinstance(vocab_to_int, dict),\
+        'vocab_to_int is not a dictionary.'
+    assert isinstance(int_to_vocab, dict),\
+        'int_to_vocab is not a dictionary.'
+    
+    # Compare lengths of dicts
+    assert len(vocab_to_int) == len(int_to_vocab),\
+        'Length of vocab_to_int and int_to_vocab don\'t match. ' \
+        'vocab_to_int is length {}. int_to_vocab is length {}'.format(len(vocab_to_int), len(int_to_vocab))
+
+    # Make sure the dicts have the same words
+    vocab_to_int_word_set = set(vocab_to_int.keys())
+    int_to_vocab_word_set = set(int_to_vocab.values())
+
+    assert not (vocab_to_int_word_set - int_to_vocab_word_set),\
+    'vocab_to_int and int_to_vocab don\'t have the same words.' \
+        '{} found in vocab_to_int, but not in int_to_vocab'.format(vocab_to_int_word_set - int_to_vocab_word_set)
+    assert not (int_to_vocab_word_set - vocab_to_int_word_set),\
+        'vocab_to_int and int_to_vocab don\'t have the same words.' \
+        '{} found in int_to_vocab, but not in vocab_to_int'.format(int_to_vocab_word_set - vocab_to_int_word_set)
+    
+    # Make sure the dicts have the same word ids
+    vocab_to_int_word_id_set = set(vocab_to_int.values())
+    int_to_vocab_word_id_set = set(int_to_vocab.keys())
+    
+    assert not (vocab_to_int_word_id_set - int_to_vocab_word_id_set),\
+        'vocab_to_int and int_to_vocab don\'t contain the same word ids.' \
+        '{} found in vocab_to_int, but not in int_to_vocab'.format(vocab_to_int_word_id_set - int_to_vocab_word_id_set)
+    assert not (int_to_vocab_word_id_set - vocab_to_int_word_id_set),\
+        'vocab_to_int and int_to_vocab don\'t contain the same word ids.' \
+        '{} found in int_to_vocab, but not in vocab_to_int'.format(int_to_vocab_word_id_set - vocab_to_int_word_id_set)
+    
+    # Make sure the dicts make the same lookup
+    missmatches = [(word, id, id, int_to_vocab[id]) for word, id in vocab_to_int.items() if int_to_vocab[id] != word]
+    
+    assert not missmatches,\
+        'Found {} missmatche(s). First missmatch: vocab_to_int[{}] = {} and int_to_vocab[{}] = {}'.format(len(missmatches),
+                                                                                                          *missmatches[0])
+    
+    assert len(vocab_to_int) > len(set(test_text))/2,\
+        'The length of vocab seems too small.  Found a length of {}'.format(len(vocab_to_int))
+    
+    _print_success_message()
+
+
+def test_tokenize(token_lookup):
+    symbols = set(['.', ',', '"', ';', '!', '?', '(', ')', '-', '\n'])
+    token_dict = token_lookup()
+    
+    # Check type
+    assert isinstance(token_dict, dict), \
+        'Returned type is {}.'.format(type(token_dict))
+
+    # Check symbols
+    missing_symbols = symbols - set(token_dict.keys())
+    unknown_symbols = set(token_dict.keys()) - symbols
+
+    assert not missing_symbols, \
+    'Missing symbols: {}'.format(missing_symbols)
+    assert not unknown_symbols, \
+        'Unknown symbols: {}'.format(unknown_symbols)
+
+    # Check values type
+    bad_value_type = [type(val) for val in token_dict.values() if not isinstance(val, str)]
+    
+    assert not bad_value_type,\
+        'Found token as {} type.'.format(bad_value_type[0])
+
+    # Check for spaces
+    key_has_spaces = [k for k in token_dict.keys() if ' ' in k]
+    val_has_spaces = [val for val in token_dict.values() if ' ' in val]
+    
+    assert not key_has_spaces,\
+        'The key "{}" includes spaces. Remove spaces from keys and values'.format(key_has_spaces[0])
+    assert not val_has_spaces,\
+    'The value "{}" includes spaces. Remove spaces from keys and values'.format(val_has_spaces[0])
+    
+    # Check for symbols in values
+    symbol_val = ()
+    for symbol in symbols:
+        for val in token_dict.values():
+            if symbol in val:
+                symbol_val = (symbol, val)
+
+    assert not symbol_val,\
+    'Don\'t use a symbol that will be replaced in your tokens. Found the symbol {} in value {}'.format(*symbol_val)
+    
+    _print_success_message()
+
+
+def test_rnn(RNN, train_on_gpu):
+    batch_size = 50
+    sequence_length = 3
+    vocab_size = 20
+    output_size=20
+    embedding_dim=15
+    hidden_dim = 10
+    n_layers = 2
+    
+    # create test RNN
+    # params: (vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
+    rnn = RNN(vocab_size, output_size, embedding_dim, hidden_dim, n_layers)
+    
+    # create test input
+    a = np.random.randint(vocab_size, size=(batch_size, sequence_length))
+    #b = torch.LongTensor(a)
+    b = torch.from_numpy(a)
+    hidden = rnn.init_hidden(batch_size)
     
     
-<link rel="stylesheet" href="/jupyter/static/components/codemirror/lib/codemirror.css?v=f25e9a9159e54b423b5a8dc4b1ab5c6e">
-<link rel="stylesheet" href="/jupyter/static/components/codemirror/addon/dialog/dialog.css?v=c89dce10b44d2882a024e7befc2b63f5">
-
-    <link rel="stylesheet" href="/jupyter/static/style/style.min.css?v=29c09309dd70e7fe93378815e5f022ae" type="text/css"/>
+    if(train_on_gpu):
+        rnn.cuda()
+        b = b.cuda()
     
-
-    <link rel="stylesheet" href="/jupyter/custom/custom.css" type="text/css" />
-    <script src="/jupyter/static/components/es6-promise/promise.min.js?v=f004a16cb856e0ff11781d01ec5ca8fe" type="text/javascript" charset="utf-8"></script>
-    <script src="/jupyter/static/components/preact/index.js?v=5b98fce8b86ce059de89f9e728e16957" type="text/javascript"></script>
-    <script src="/jupyter/static/components/proptypes/index.js?v=c40890eb04df9811fcc4d47e53a29604" type="text/javascript"></script>
-    <script src="/jupyter/static/components/preact-compat/index.js?v=d376eb109a00b9b2e8c0d30782eb6df7" type="text/javascript"></script>
-    <script src="/jupyter/static/components/requirejs/require.js?v=6da8be361b9ee26c5e721e76c6d4afce" type="text/javascript" charset="utf-8"></script>
-    <script>
-      require.config({
-          
-          urlArgs: "v=20181206115426",
-          
-          baseUrl: '/jupyter/static/',
-          paths: {
-            'auth/js/main': 'auth/js/main.min',
-            custom : '/jupyter/custom',
-            nbextensions : '/jupyter/nbextensions',
-            kernelspecs : '/jupyter/kernelspecs',
-            underscore : 'components/underscore/underscore-min',
-            backbone : 'components/backbone/backbone-min',
-            jquery: 'components/jquery/jquery.min',
-            bootstrap: 'components/bootstrap/js/bootstrap.min',
-            bootstraptour: 'components/bootstrap-tour/build/js/bootstrap-tour.min',
-            'jquery-ui': 'components/jquery-ui/ui/minified/jquery-ui.min',
-            moment: 'components/moment/moment',
-            codemirror: 'components/codemirror',
-            termjs: 'components/xterm.js/dist/xterm',
-            typeahead: 'components/jquery-typeahead/dist/jquery.typeahead.min',
-          },
-          map: { // for backward compatibility
-              "*": {
-                  "jqueryui": "jquery-ui",
-              }
-          },
-          shim: {
-            typeahead: {
-              deps: ["jquery"],
-              exports: "typeahead"
-            },
-            underscore: {
-              exports: '_'
-            },
-            backbone: {
-              deps: ["underscore", "jquery"],
-              exports: "Backbone"
-            },
-            bootstrap: {
-              deps: ["jquery"],
-              exports: "bootstrap"
-            },
-            bootstraptour: {
-              deps: ["bootstrap"],
-              exports: "Tour"
-            },
-            "jquery-ui": {
-              deps: ["jquery"],
-              exports: "$"
-            }
-          },
-          waitSeconds: 30,
-      });
-
-      require.config({
-          map: {
-              '*':{
-                'contents': 'services/contents',
-              }
-          }
-      });
-
-      // error-catching custom.js shim.
-      define("custom", function (require, exports, module) {
-          try {
-              var custom = require('custom/custom');
-              console.debug('loaded custom.js');
-              return custom;
-          } catch (e) {
-              console.error("error loading custom.js", e);
-              return {};
-          }
-      })
-    </script>
-
+    output, hidden_out = rnn(b, hidden)
     
+    assert_test = AssertTest({
+                             'Input Size': vocab_size,
+                             'Output Size': output_size,
+                             'Hidden Dim': hidden_dim,
+                             'N Layers': n_layers,
+                             'Batch Size': batch_size,
+                             'Sequence Length': sequence_length,
+                             'Input': b})
     
-
-</head>
-
-<body class="edit_app "
- 
-data-base-url="/jupyter/"
-data-file-path="project-tv-script-generation/problem_unittests.py"
-
-  
- 
-
-dir="ltr">
-
-<noscript>
-    <div id='noscript'>
-      Jupyter Notebook requires JavaScript.<br>
-      Please enable it to proceed.
-  </div>
-</noscript>
-
-<div id="header">
-  <div id="header-container" class="container">
-  <div id="ipython_notebook" class="nav navbar-brand pull-left"><a href="/jupyter/tree" title='dashboard'><img src='/jupyter/static/base/images/logo.png?v=641991992878ee24c6f3826e81054a0f' alt='Jupyter Notebook'/></a></div>
-
-  
-  
-  
-
-    <span id="login_widget">
-      
-    </span>
-
-  
-
-  
-
-  
-
-<span id="save_widget" class="pull-left save_widget">
-    <span class="filename"></span>
-    <span class="last_modified"></span>
-</span>
-
-
-  </div>
-  <div class="header-bar"></div>
-
-  
-
-<div id="menubar-container" class="container">
-  <div id="menubar">
-    <div id="menus" class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-          <p  class="navbar-text indicator_area">
-          <span id="current-mode" >current mode</span>
-          </p>
-        <button type="button" class="btn btn-default navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <i class="fa fa-bars"></i>
-          <span class="navbar-text">Menu</span>
-        </button>
-        <ul class="nav navbar-nav navbar-right">
-          <li id="notification_area"></li>
-        </ul>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">File</a>
-              <ul id="file-menu" class="dropdown-menu">
-                <li id="new-file"><a href="#">New</a></li>
-                <li id="save-file"><a href="#">Save</a></li>
-                <li id="rename-file"><a href="#">Rename</a></li>
-                <li id="download-file"><a href="#">Download</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Edit</a>
-              <ul id="edit-menu" class="dropdown-menu">
-                <li id="menu-find"><a href="#">Find</a></li>
-                <li id="menu-replace"><a href="#">Find &amp; Replace</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Key Map</li>
-                <li id="menu-keymap-default"><a href="#">Default<i class="fa"></i></a></li>
-                <li id="menu-keymap-sublime"><a href="#">Sublime Text<i class="fa"></i></a></li>
-                <li id="menu-keymap-vim"><a href="#">Vim<i class="fa"></i></a></li>
-                <li id="menu-keymap-emacs"><a href="#">emacs<i class="fa"></i></a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">View</a>
-              <ul id="view-menu" class="dropdown-menu">
-              <li id="toggle_header" title="Show/Hide the logo and notebook title (above menu bar)">
-              <a href="#">Toggle Header</a></li>
-              <li id="menu-line-numbers"><a href="#">Toggle Line Numbers</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Language</a>
-              <ul id="mode-menu" class="dropdown-menu">
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="lower-header-bar"></div>
-
-
-</div>
-
-<div id="site">
-
-
-<div id="texteditor-backdrop">
-<div id="texteditor-container" class="container"></div>
-</div>
-
-
-</div>
-
-
-
-
-
-
+    # initialization
+    correct_hidden_size = (n_layers, batch_size, hidden_dim)
+    assert_condition = hidden[0].size() == correct_hidden_size
+    assert_message = 'Wrong hidden state size. Expected type {}. Got type {}'.format(correct_hidden_size, hidden[0].size())
+    assert_test.test(assert_condition, assert_message)
     
+    # output of rnn
+    correct_hidden_size = (n_layers, batch_size, hidden_dim)
+    assert_condition = hidden_out[0].size() == correct_hidden_size
+    assert_message = 'Wrong hidden state size. Expected type {}. Got type {}'.format(correct_hidden_size, hidden_out[0].size())
+    assert_test.test(assert_condition, assert_message)
+    
+    correct_output_size = (batch_size, output_size)
+    assert_condition = output.size() == correct_output_size
+    assert_message = 'Wrong output size. Expected type {}. Got type {}'.format(correct_output_size, output.size())
+    assert_test.test(assert_condition, assert_message)
+    
+    _print_success_message()
 
 
-<script src="/jupyter/static/edit/js/main.min.js?v=7eb6af843396244a81afb577aedbaf89" type="text/javascript" charset="utf-8"></script>
-
-
-<script type='text/javascript'>
-  function _remove_token_from_url() {
-    if (window.location.search.length <= 1) {
-      return;
-    }
-    var search_parameters = window.location.search.slice(1).split('&');
-    for (var i = 0; i < search_parameters.length; i++) {
-      if (search_parameters[i].split('=')[0] === 'token') {
-        // remote token from search parameters
-        search_parameters.splice(i, 1);
-        var new_search = '';
-        if (search_parameters.length) {
-          new_search = '?' + search_parameters.join('&');
-        }
-        var new_url = window.location.origin + 
-                      window.location.pathname + 
-                      new_search + 
-                      window.location.hash;
-        window.history.replaceState({}, "", new_url);
-        return;
-      }
-    }
-  }
-  _remove_token_from_url();
-</script>
-</body>
-
-</html>
+def test_forward_back_prop(RNN, forward_back_prop, train_on_gpu):
+    batch_size = 200
+    input_size = 20
+    output_size = 10
+    sequence_length = 3
+    embedding_dim=15
+    hidden_dim = 10
+    n_layers = 2
+    learning_rate = 0.01
+    
+    # create test RNN
+    rnn = RNN(input_size, output_size, embedding_dim, hidden_dim, n_layers)
+    
+    mock_decoder = MagicMock(wraps=_TestNN(input_size, output_size))
+    if train_on_gpu:
+        mock_decoder.cuda()
+    
+    mock_decoder_optimizer = MagicMock(wraps=torch.optim.Adam(mock_decoder.parameters(), lr=learning_rate))
+    mock_criterion = MagicMock(wraps=torch.nn.CrossEntropyLoss())
+    
+    with patch.object(torch.autograd, 'backward', wraps=torch.autograd.backward) as mock_autograd_backward:
+        inp = torch.FloatTensor(np.random.rand(batch_size, input_size))
+        target = torch.LongTensor(np.random.randint(output_size, size=batch_size))
+        
+        hidden = rnn.init_hidden(batch_size)
+        
+        loss, hidden_out = forward_back_prop(mock_decoder, mock_decoder_optimizer, mock_criterion, inp, target, hidden)
+    
+    assert (hidden_out[0][0]==hidden[0][0]).sum()==batch_size*hidden_dim, 'Returned hidden state is the incorrect size.'
+    
+    assert mock_decoder.zero_grad.called or mock_decoder_optimizer.zero_grad.called, 'Didn\'t set the gradients to 0.'
+    assert mock_decoder.forward_called, 'Forward propagation not called.'
+    assert mock_autograd_backward.called, 'Backward propagation not called'
+    assert mock_decoder_optimizer.step.called, 'Optimization step not performed'
+    assert type(loss) == float, 'Wrong return type. Expected {}, got {}'.format(float, type(loss))
+    
+    _print_success_message()
